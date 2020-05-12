90d72cee6dbd71ca32ad5d720908c34738a88951
Alan Ayala <aaga941228@outlook.es>
2020-05-12 18:46:00 -0500

    htmk and css added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(express.static(path.resolve(__dirname, '../public')))
 
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
   console.log(`server on port ${app.get('port')}`)
+})
+
+const io = SocketIo(server)
+
+io.on('connection', (socket) => {
+  console.log('new connection: ', socket.id)
 })
