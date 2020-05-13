b4cb9e20691347a6205fbc08f29d52dab65f23c9
Alan Ayala <aaga941228@outlook.es>
2020-05-12 19:16:31 -0500

    chat done

--- public/app.js
+++ public/app.js
@@ -1,1 +1,33 @@
-const socket = io()
+const socket = io();
+
+const message = document.querySelector("#message");
+const username = document.querySelector("#username");
+const btn = document.querySelector("#send");
+const output = document.querySelector("#output");
+const actions = document.querySelector("#actions");
+
+btn.addEventListener("click", () => {
+  socket.emit("chat:message", {
+    username: username.value,
+    message: message.value,
+  });
+});
+
+message.addEventListener("keypress", function () {
+  socket.emit("chat:typing", username.value);
+});
+
+socket.on("chat:message", function (data) {
+  actions.innerHTML = "";
+  output.innerHTML += `
+    <p>
+    <strong>${data.username}</strong>: ${data.message}
+    </p>
+  `;
+});
+
+socket.on("chat:typing", function (data) {
+  actions.innerHTML = `
+    <p><em>${data}</em> is typing</p>
+  `;
+});
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const SocketIo = require('socket.io')
-const path = require('path')
+const express = require("express");
+const SocketIo = require("socket.io");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.set('port', process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 
-app.use(express.static(path.resolve(__dirname, '../public')))
+app.use(express.static(path.resolve(__dirname, "../public")));
 
-const server = app.listen(app.get('port'), () => {
-  console.log(`server on port ${app.get('port')}`)
-})
+const server = app.listen(app.get("port"), () => {
+  console.log(`server on port ${app.get("port")}`);
+});
 
-const io = SocketIo(server)
+const io = SocketIo(server);
 
-io.on('connection', (socket) => {
-  console.log('new connection: ', socket.id)
-})
+io.on("connection", (socket) => {
+  console.log("new connection: ", socket.id);
+
+  socket.on("chat:message", (data) => {
+    io.sockets.emit("chat:message", data);
+  });
+
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
+});
